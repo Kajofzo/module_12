030073e2077429dc11affeaa50f63c96265682ba
Kajofzo <108760962+Kajofzo@users.noreply.github.com>
2023-10-02 11:02:49 +0200

    done

---
+++ Python/workshop1/script1/epic.py
@@ -0,0 +1,13 @@
+import pyautogui
+#control mouse and keyboard
+import webbrowser
+#can open webbrowser
+import time
+#time
+
+webbrowser.open('https://skibiditoiletorcreeper.nl')
+#opens website
+time.sleep(2)
+#waits 2 seconds
+pyautogui.click(650, 500)
+#clicks on the website
