99b35fed46b0daf2dcf69173b916735d1a8ab41b
Kajofzo <108760962+Kajofzo@users.noreply.github.com>
2023-10-02 11:08:36 +0200

    comments

--- Python/workshop1/script2/virus.py
+++ Python/workshop1/script2/virus.py
@@ -1,10 +1,16 @@
 import datetime
-
+#imports the datetime module
 Current_Date = datetime.datetime.today()
+#gets the current date
 print ('Current Date: ' + str(Current_Date))
+#converts the current date into a string
 
 Previous_Date = datetime.datetime.today() - datetime.timedelta(days=1)
+#gets the previous date by subtracting 1 day
 print ('Previous Date: ' + str(Previous_Date))
+#converts the previous date into a string
 
 NextDay_Date = datetime.datetime.today() + datetime.timedelta(days=1)
+#gets the next date by adding 1 day
 print ('Next Date: ' + str(NextDay_Date))
+#converts the next date into a string
